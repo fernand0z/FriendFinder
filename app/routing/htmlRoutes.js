8e6fc70da57ca7e7b4f02207fb6a36b330987210
Fernando <fernandoz@outlook.com>
2018-04-25 12:37:22 -0700

    FIXED ROUTING

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,54 +1,31 @@
 // JS File to handle possible routes for Friend Finder App
 // Developed by Fernando Zacarias
 
-
 // DEPENDENCIES
 // =============================================================
 const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Routes
 // =============================================================
 function htmlRoutes(app) {
-// Basic route that sends the user first to the AJAX Page
-app.use(function (req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
-});
 
 // Route to survey page 
 app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
 });
+// Basic route that sends the user first to the AJAX Page
+app.use(function (req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+});
+
 };
 
 //Export for server.js file
 module.exports = htmlRoutes
-
-
-
-
-// **************************************************************************************************
-// console.log('HTML Route Connected Successfully');
-
-
-// // Node Dependencies
-// var path = require('path');
-
-
-// // Includes Two Routes
-// function htmlRoutes(app) {
-
-//   // A GET Route to /survey which should display the survey page.
-//   app.get('/survey', function (req, res) {
-//     res.sendFile(path.join(__dirname + '/../public/survey.html'));
-//   });
-
-//   // A default USE route that leads to home.html which displays the home page.
-//   app.use(function (req, res) {
-//     res.sendFile(path.join(__dirname + '/../public/home.html'));
-//   });
-
-// }
-
-
-// // Export for use in main server.js file
-// module.exports = htmlRoutes;
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(express.static(path.join(__dirname, 'app/public')));
 //Server routing
 apiRoutes(app); 
 htmlRoutes(app); 
