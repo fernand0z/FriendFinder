4d15d6b6c65cd2cac7b3e168e3bd59f396bb31be
Fernando <fernandoz@outlook.com>
2018-04-25 22:06:12 -0700

    Added score comparison logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,5 @@
+import { STATUS_CODES } from 'http';
+
 // JS File to handle possible api routes
 // Developed by Fernando Zacarias
 
@@ -16,25 +18,57 @@ app.use(bodyParser.json());
 //GET route for friends
 const friendsData = require('./../data/friends.js');
 function apiRoutes(app) {
-app.get("/api/friends", function (req, res) {
-    return res.json(friendsData);
-});
+    app.get("/api/friends", function (req, res) {
+        return res.json(friendsData);
+    });
 
-//Friends POST - takes in JSON input
-app.post("/api/friends", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body-parser middleware
-    var newFriend = req.body;
+    //Friends POST - takes in JSON input
+    app.post("/api/friends", function (req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body-parser middleware
+        var newFriend = req.body;
 
-    //Replace method to remove all spaces, concatenate string sections, set all to lowercase values
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+        //Replace method to remove all spaces, concatenate string sections, set all to lowercase values
+        newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
 
-    console.log(newFriend);
+        console.log(newFriend);
 
-    friendsData.push(newFriend);
+        friendsData.push(newFriend);
 
-    res.json(newFriend);
-});
+        let newFriendData = {
+            name: newFriend.name,
+            photo: newFriend.photo,
+            scores: []
+        };
+        console.log(newFriendData);
+
+        //Declare a variable to hold the scores in an array
+        let scoreArray = [];
+        //Loop to add each of the user scores into the array
+        for (var i = 0; i < req.body.scores.length; i++) {
+            scoreArray.push(parseInt(req.body.scores[i]));
+        }
+
+        //Assign array to scores key in newFriendData
+        newFriendData.scores = scoreArray;
+
+        //Setup the process to compare scores
+        let comparisonArray = [];
+        //Loop to compare for each set of scores in the friendData array
+        for (var j = 0; j < friendsData.scores.length; j++) {
+            //Declare variables to hold the comparison results
+            let instanceComparison = 0;
+            for (var k = 0; k < newFriendData.scores.length; k++) {
+
+                instanceComparison += Math.abs(newFriendData.scores[j] - friendsData[j].scores[k]);
+            }
+            //Push difference value to comparisonArray
+            comparisonArray.push(instanceComparison);
+        }
+
+
+        res.json(newFriend);
+    });
 };
 
 //Export api route for friendsData
